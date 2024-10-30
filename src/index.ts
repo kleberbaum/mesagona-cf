95b5b882fdd49ae3fa040d3302f2d7e26b660cd9
Florian Kleber <kleber@snek.at>
2024-10-30 06:12:11 +0000

    feat: add basic mesagona recommendation system

--- src/index.ts
+++ src/index.ts
@@ -46,8 +46,35 @@ export const graphql = {
 
       const ai = new Ai((ctx.env as any).AI);
 
+      // const AiResponse = await ai.run("@cf/mistral/mistral-7b-instruct-v0.2-lora", {
+      //   prompt,
+      //   //messages: [{"role": "user", "content": "Hello world"}],
+      //   //raw: true, //skip applying the default chat template
+      //   //lora: "cc7f481e-8b70-45bd-b2a8-d39fc05511f9", //the finetune id OR name
+      // });
+      // const AiResponse = await ai.run("@cf/mistral/mistral-7b-instruct-v0.1", {
+      //   raw: true,
+      //   messages: [
+      //     {
+      //       "role": "user",
+      //       "content": "Summarize the following: Some newspapers, TV channels and well-known companies publish false news stories to fool people on 1 April. One of the earliest examples of this was in 1957 when a programme on the BBC, the UKs national TV channel, broadcast a report on how spaghetti grew on trees. The film showed a family in Switzerland collecting spaghetti from trees and many people were fooled into believing it, as in the 1950s British people didnt eat much pasta and many didnt know how it was made! Most British people wouldnt fall for the spaghetti trick today, but in 2008 the BBC managed to fool their audience again with their Miracles of Evolution trailer, which appeared to show some special penguins that had regained the ability to fly. Two major UK newspapers, The Daily Telegraph and the Daily Mirror, published the important story on their front pages."
+      //     }
+      //   ],
+      //   lora: "cf-public-cnn-summarization"
+      // });
       const AiResponse = await ai.run("@cf/meta/llama-3.1-8b-instruct", {
-        prompt,
+        //raw: true,
+        messages: [
+          {
+            "role": "system",
+            "content": 'Only provide a populated json in this format {"ingredients": [], "recommendation_text": ""}'
+          },
+          {
+            "role": "user",
+            "content": prompt
+          }
+        ],
+        //lora: "cf-public-cnn-summarization"
       });
 
       return AiResponse.response;
